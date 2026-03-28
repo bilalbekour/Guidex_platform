2a366aaf2693b1048e4cf96a8da9e50612f47d6f
Bilal Bekour <bilibekour@gmail.com>
2026-03-28 19:13:03 +0100

    Update settings.py

--- guidex_backend/guidex_backend/settings.py
+++ guidex_backend/guidex_backend/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-kt9xgj$w-_(+p41rhud$(&xzj499@^mq_&8p^ulhdcb@l5u9+v'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
